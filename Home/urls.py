c121593a6bff2b4d7c5e6f5183219f2723d73823
pramod <gopinath.pramod@gmail.com>
2024-10-02 17:20:38 +0530

    forgot password

--- Home/urls.py
+++ Home/urls.py
@@ -20,6 +20,14 @@ urlpatterns = [
     path('change_password', views.change_password, name='change_password'),
     path('password_change_done', views.password_change_done, name='password_change_done'),
     path("TeamsAndConditions",views.TeamsAndConditions,name="TeamsAndConditions"),
-    path("PrivacyPolicy",views.PrivacyPolicy,name="PrivacyPolicy")
+    path("PrivacyPolicy",views.PrivacyPolicy,name="PrivacyPolicy"),
+
+
+    # password reset
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     
 ]
